8ac9cf9eae22dfd3c649ee0054362638956a8bda
Jenise Ruehle <jela07@gmail.com>
2020-10-11 19:48:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
   
   post '/piglatinize' do
-    @piglatinized = PigLatinizer.new.piglatinize(params[:user_phrase].to_s)
+    @piglatinized = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :piglatinize
   end 
   
