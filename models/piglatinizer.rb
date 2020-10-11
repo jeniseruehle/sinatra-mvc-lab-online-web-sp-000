4911726c133a90e6d0b48c2c3d58018ad8d24c22
Jenise Ruehle <jela07@gmail.com>
2020-10-11 20:02:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,5 +4,10 @@ class PigLatinizer
     piglatin = phrase.split(" ")
     piglatinized = []
     
+    piglatin.each do |words|
+      vowel = word.scan(/[aAeEiIoOuU]/).first
+      phrase = word.split(vowel, 2)
+      if phrase[0] == ""
+        piglatinize = "#{vowel}"
 
 end 
